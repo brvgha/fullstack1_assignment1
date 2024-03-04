938ed732f0ffef6b230b7c28c21b32add07b8d40
Brugha Fitzpatrick <brughafitzpatrick@gmail.com>
2024-03-04 21:42:54 +0000

    added json storage

--- src/models/db.js
+++ src/models/db.js
@@ -1,3 +1,4 @@
+// moving from the memory structure to the json storage
 // import { userMemStore } from "./mem/user-mem-store.js";
 // import { placeMarkMemStore } from "./mem/placemark-mem-store.js";
 // import { infoMemStore } from "./mem/info-mem-store.js";
---
+++ src/models/json/info-json-store.js
@@ -0,0 +1,47 @@
+import { v4 } from "uuid";
+import { db } from "./store-utils.js";
+
+export const infoJsonStore = {
+  async getAllInfos() {
+    await db.read();
+    return db.data.infos;
+  },
+
+  async addInfo(placemarkId, info) {
+    await db.read();
+    info._id = v4();
+    info.placemarkid = placemarkId;
+    db.data.infos.push(info);
+    await db.write();
+    return info;
+  },
+
+  async getInfosByPlaylistId(id) {
+    await db.read();
+    return db.data.infos.filter((info) => info.placemarkid === id);
+  },
+
+  async getInfoById(id) {
+    await db.read();
+    return db.data.infos.find((info) => info._id === id);
+  },
+
+  async deleteInfo(id) {
+    await db.read();
+    const index = db.data.infos.findIndex((info) => info._id === id);
+    db.data.infos.splice(index, 1);
+    await db.write();
+  },
+
+  async deleteAllInfos() {
+    db.data.infos = [];
+    await db.write();
+  },
+
+  async updateInfo(info, updatedInfo) {
+    info.category = updatedInfo.category;
+    info.description = updatedInfo.description;
+    info.analytics = updatedInfo.analytics;
+    await db.write();
+  },
+};
