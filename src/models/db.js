c4b684d4d8671fb82425a555299e5cbb338a2427
Brugha Fitzpatrick <brughafitzpatrick@gmail.com>
2024-03-05 16:10:21 +0000

    addition of mongo and testing

--- src/models/db.js
+++ src/models/db.js
@@ -7,14 +7,29 @@ import { userJsonStore } from "./json/user-json-store.js";
 import { placeMarkJsonStore } from "./json/placemark-json-store.js";
 import { infoJsonStore } from "./json/info-json-store.js";
 
+import { connectMongo } from "./mongo/connect.js";
+import { userMongoStore } from "./mongo/user-mongo-store.js";
+
 export const db = {
   userStore: null,
   placeMarkStore: null,
   infoStore: null,
 
-  init() {
-    this.userStore = userJsonStore;
-    this.placeMarkStore = placeMarkJsonStore;
-    this.infoStore = infoJsonStore;
+  init(storeType) {
+    switch (storeType) {
+      case "json":
+        this.userStore = userJsonStore;
+        this.placeMarkStore = placeMarkJsonStore;
+        this.infoStore = infoJsonStore;
+        break;
+      case "mongo":
+        this.userStore = userMongoStore;
+        connectMongo();
+        break;
+      default:
+        this.userStore = userMemStore;
+        this.placeMarkStore = placeMarkMemStore;
+        this.infoStore = infoMemStore;
+    }
   },
 };
--- test/user-model-test.js
+++ test/user-model-test.js
@@ -1,11 +1,12 @@
 import { assert } from "chai";
 import { db } from "../src/models/db.js";
 import { maggie, testUsers } from "./fixtures.js";
+import { assertSubset } from "./test-utils.js";
 
 suite("User API tests", () => {
 
   setup(async () => {
-    db.init("json");
+    db.init("mongo");
     await db.userStore.deleteAll();
     for (let i = 0; i < testUsers.length; i += 1) {
       // eslint-disable-next-line no-await-in-loop
@@ -15,7 +16,7 @@ suite("User API tests", () => {
 
   test("create a user", async () => {
     const newUser = await db.userStore.addUser(maggie);
-    assert.deepEqual(maggie, newUser)
+    assertSubset(maggie, newUser)
   });
 
   test("delete all users", async () => {
