b32289d5ac2b9a2a86a7070b0f30c8f8a9086141
Brugha Fitzpatrick <64151810+brvgha@users.noreply.github.com>
2024-04-21 22:39:12 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -43,6 +43,7 @@ const swaggerOptions = {
 async function init() {
   const server = Hapi.server({
     port: process.env.PORT || 3000,
+    host: "0.0.0.0",
   });
 
   // await server.register(Vision);
